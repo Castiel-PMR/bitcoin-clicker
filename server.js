19f4f9a98a1a8e4c56b14b9a80ae971c62323bad
Iurii <116222684+Castiel-PMR@users.noreply.github.com>
2024-07-07 15:57:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,84 +1,148 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
+const API_BASE_URL = "https://c022-95-153-90-59.ngrok-free.app"; // Замените на ваш URL ngrok
+let count = 0.0;
+let lastClickTime = 0;
+const clickInterval = 150;
+const incrementValue = 0.001;
+let clickCount = 0;
+const minBonusClicks = 20;
+const maxBonusClicks = 40;
+let nextBonusClicks = getRandomClicks(minBonusClicks, maxBonusClicks);
+let bonusActive = false;
 
-const app = express();
-const port = 3000;
+function getUserIdFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('user_id');
+}
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+function getRandomClicks(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-// Путь к базе данных
-const dbPath = path.join(__dirname, 'data', 'database.db');
+const userId = getUserIdFromUrl();
+if (!userId) {
+    alert("User ID not found!");
+    throw new Error("User ID not found");
+}
 
-// Создание и подключение к базе данных SQLite
-const db = new sqlite3.Database(dbPath, (err) => {
-    if (err) {
-        console.error('Could not connect to database', err);
-    } else {
-        console.log('Connected to SQLite database');
+const clickArea = document.getElementById('click-area');
+const pickaxe = document.getElementById('pickaxe');
+const rock = document.getElementById('rock');
+const counter = document.getElementById('counter');
+const clickSound = document.getElementById('click-sound');
+const sparks = document.getElementById('sparks');
+const bonusMessage = document.getElementById('bonus-message');
+const bonusSound = new Audio('bonus.mp3'); // Добавьте файл bonus.mp3 в проект
+
+async function loadUserData() {
+    try {
+        console.log('Loading user data...');
+        const response = await fetch(`${API_BASE_URL}/api/load/${userId}`, {
+            headers: {
+                'ngrok-skip-browser-warning': 'true',
+                'Content-Type': 'application/json'
+            }
+        });
+        const userData = await response.json();
+        count = userData.btcCount;
+        counter.textContent = `BTC: ${count.toFixed(3)}`;
+        console.log('User data loaded:', userData);
+    } catch (error) {
+        console.error('Error loading user data:', error);
+    }
+}
+
+async function saveUserData() {
+    try {
+        console.log('Saving user data...');
+        const response = await fetch(`${API_BASE_URL}/api/save`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'ngrok-skip-browser-warning': 'true'
+            },
+            body: JSON.stringify({ telegramId: userId, btcCount: count }),
+        });
+        const userData = await response.json();
+        console.log('User data saved:', userData);
+    } catch (error) {
+        console.error('Error saving user data:', error);
+    }
+}
+
+function showBonusMessage(bonus) {
+    bonusMessage.textContent = `+ Бонус: ${bonus.toFixed(3)} 💰`;
+    bonusMessage.classList.add('bonus-animation');
+    bonusSound.play();
+    setTimeout(() => {
+        bonusMessage.classList.remove('bonus-animation');
+        bonusMessage.style.opacity = 0;
+        bonusActive = false; // Разрешаем клики после анимации
+    }, 4500); // 1.5 сек на анимацию и 3 сек на замер
+}
+
+function openFullscreen() {
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+        document.documentElement.msRequestFullscreen();
+    }
+}
+
+clickArea.addEventListener('click', () => {
+    if (bonusActive) return; // Блокируем клики, если бонус активен
+
+    const currentTime = new Date().getTime();
+    if (currentTime - lastClickTime < clickInterval) {
+        return;
+    }
+
+    lastClickTime = currentTime;
+    clickCount++;
+    count += incrementValue;
+
+    // Добавление бонуса
+    if (clickCount >= nextBonusClicks) {
+        const bonus = (Math.random() * 1) + 0.05; // Генерация случайного бонуса от 0.05 до 1.05
+        count += bonus;
+        clickCount = 0; // Сброс счетчика кликов
+        nextBonusClicks = getRandomClicks(minBonusClicks, maxBonusClicks); // Обновление количества кликов до следующего бонуса
+        bonusActive = true; // Устанавливаем флаг, что бонус активен
+        showBonusMessage(bonus);
+    }
+
+    counter.textContent = `BTC: ${count.toFixed(3)}`;
+    clickSound.currentTime = 0;
+    clickSound.play();
+    saveUserData();
+    sparks.style.opacity = '1';
+    sparks.style.transform = 'translate(-50%, -50%) scale(1)';
+    sparks.classList.add('spark-animation');
+    pickaxe.style.transform = 'translate(-50%, 0) rotate(45deg)';
+
+    setTimeout(() => {
+        pickaxe.style.transform = 'translate(-50%, 0) rotate(0deg)';
+        sparks.style.opacity = '0';
+        sparks.classList.remove('spark-animation');
+    }, 100);
+
+    rock.style.transform = 'translateX(-50%) rotate(-3deg)';
+    setTimeout(() => {
+        rock.style.transform = 'translateX(-50%) rotate(3deg)';
+        setTimeout(() => {
+            rock.style.transform = 'translateX(-50%) rotate(0deg)';
+        }, 100);
+    }, 100);
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Проверка, что мы в Telegram WebApp
+    if (window.Telegram.WebApp) {
+        openFullscreen();
     }
 });
 
-// Создание таблицы пользователей, если она не существует
-db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS users (
-        telegramId TEXT PRIMARY KEY,
-        btcCount REAL DEFAULT 0
-    )`, (err) => {
-        if (err) {
-            console.error('Error creating table:', err);
-        } else {
-            console.log('User table created or already exists.');
-        }
-    });
-});
-
-// Маршрут для корневого URL
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// Маршрут для сохранения данных пользователя
-app.post('/api/save', (req, res) => {
-    const { telegramId, btcCount } = req.body;
-    console.log('Received save request:', telegramId, btcCount);
-    if (!telegramId) {
-        return res.status(400).json({ error: 'Telegram ID is required' });
-    }
-    db.run(`INSERT INTO users (telegramId, btcCount) VALUES (?, ?)
-            ON CONFLICT(telegramId) DO UPDATE SET btcCount = excluded.btcCount`, [telegramId, btcCount], function (err) {
-        if (err) {
-            console.error('Error saving user data:', err);
-            return res.status(500).json({ error: 'Internal server error' });
-        }
-        res.json({ message: 'User data saved successfully' });
-    });
-});
-
-// Маршрут для получения данных пользователя
-app.get('/api/load/:telegramId', (req, res) => {
-    const { telegramId } = req.params;
-    console.log('Received load request:', telegramId);
-    db.get(`SELECT btcCount FROM users WHERE telegramId = ?`, [telegramId], (err, row) => {
-        if (err) {
-            console.error('Error loading user data:', err);
-            return res.status(500).json({ error: 'Internal server error' });
-        }
-        if (row) {
-            console.log('User data found:', row);
-            res.json({ btcCount: row.btcCount });
-        } else {
-            console.log('User data not found, returning 0 BTC.');
-            res.json({ btcCount: 0 });
-        }
-    });
-});
-
-// Запуск сервера
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+loadUserData();
