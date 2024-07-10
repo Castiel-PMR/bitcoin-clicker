5b2f9681ae4d8bafd090854e9e75142e46a479a3
Castiel-PMR <Castiel-PMR@users.noreply.github.com>
2024-07-10 11:05:32 +0000

    deploy: ab2fe0f0534744927a0834914c65d5346c476296

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://9bc6-95-153-90-59.ngrok-free.app"; // Замените на ваш URL ngrok
+const API_BASE_URL = "https://e85a-95-153-90-59.ngrok-free.app"; // Замените на ваш URL ngrok
 let count = 0.0;
 let lastClickTime = 0;
 const clickInterval = 150;
